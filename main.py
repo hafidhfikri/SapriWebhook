9b457a9b677c12b4d40cec8ebd6c518def85da1f
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 19:51:19 +0700

    Phase 1.0

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def webhook(request):
     json_parse = request.get_json() #jadi dictionary
     print(type(json_parse)) 
     print(type(request))
+    print(json_parse.get('originalDetectIntentRequest', {}).get('source') is 'telegram')
+    print(json_parse.get('originalDetectIntentRequest', {}).get('source') == 'telegram')
     
     #dialogflow and telegram validation
-    if ('responseId' in json_parse) and (json_parse.get('originalDetectIntentRequest', {}).get('source') is 'telegram') :
+    if ('responseId' in json_parse) and (json_parse.get('originalDetectIntentRequest', {}).get('source') == 'telegram') :
         '''
         try:
             url = ""
