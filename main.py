d9ec83f1594d309187c77c527121804bc4bda739
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 17:09:05 +0700

    debuging

--- main.py
+++ main.py
@@ -3,28 +3,25 @@ import json
 
 def webhook(request):
     #set default response
-    print("START")
     response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Internal Error']}}]})
-    '''
+    
     #get request payload
     request_json = request.get_json()
     json_parse = json.loads(request_json)
 
     #dialogflow and telegram validation
     if ("responseId" in json_parse) and (json_parse["originalDetectIntentRequest"]["source"] == "telegram") :
-        print("ACCEPTED!!!")
-        
+        '''
         try:
             url = ""
             response = req.post(url,request_json)
         except:
             print("Error Something Happen....")
-            response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Internal Error"]}}]})
+            response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Internal Error']}}]})
+        '''
         
-        print("Sukses")
-        response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Sukses"]}}]})
+        response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Sukses']}}]})
     else:
-        print("Illegal Request")
-        response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Illegal Request"]}}]})
-    '''
+        response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Illegal Action']}}]})
+    
     return response
