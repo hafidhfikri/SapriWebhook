7326568d80c613bee08a45644b96ab41adc991a9
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 16:51:16 +0700

    debuging

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import json
 
 def webhook(request):
     #set default response
+    print("START")
     response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Internal Error"]}}]})
-
+    '''
     #get request payload
     request_json = request.get_json()
     json_parse = json.loads(request_json)
@@ -12,18 +13,18 @@ def webhook(request):
     #dialogflow and telegram validation
     if ("responseId" in json_parse) and (json_parse["originalDetectIntentRequest"]["source"] == "telegram") :
         print("ACCEPTED!!!")
-        '''
+        
         try:
             url = ""
             response = req.post(url,request_json)
         except:
             print("Error Something Happen....")
             response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Internal Error"]}}]})
-        '''
+        
         print("Sukses")
         response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Sukses"]}}]})
     else:
         print("Illegal Request")
         response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Illegal Request"]}}]})
-
+    '''
     return response
