e191850b08a1af14fe0d3d79c22b0e7e3876a262
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 15:48:21 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import request
+import json
+
+def webhook(request):
+    #set default response
+    response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Internal Error"]}}]})
+
+    #get request payload
+    request_json = request.get_json()
+    json_parse = json.loads(request_json)
+
+    #dialogflow and telegram validation
+    if ("responseId" in json_parse) and (json_parse["originalDetectIntentRequest"]["source"] == "telegram") :
+        print("ACCEPTED!!!")
+        try:
+            url = ""
+            response = request.post(url,request_json)
+        except:
+            print("Error Something Happen....")
+            response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Internal Error"]}}]})
+    else:
+        print("Illegal Request")
+        response = json.dumps({"fulfillmentMessages":[{"text":{"text":["Illegal Request"]}}]})
+
+    return response
