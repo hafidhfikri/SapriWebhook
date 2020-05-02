dccfd64d7983abd69701f7e8348d09bf93386c74
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 17:32:16 +0700

    Phase 1.0

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import requests as req
 import json
 
-def webhook(rere):
+def webhook(request):
     #set default response
     response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Internal Error']}}]})
     
     #get request payload
-    json_parse = rere
-
+    json_parse = json.loads(request)
+    '''
     #dialogflow and telegram validation
     if ('responseId' in json_parse) and (json_parse['originalDetectIntentRequest']['source'] == 'telegram') :
-        '''
+        
         try:
             url = ""
             response = req.post(url,request)
         except:
             print("Error Something Happen....")
             response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Internal Error']}}]})
-        '''
+        
 
         response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Sukses']}}]})
     else:
         response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Illegal Action']}}]})
-    
+    '''
     return response  
