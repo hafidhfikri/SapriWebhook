45a51ffd5cf732d4911e9566d5e578762d09d3c4
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 20:38:14 +0700

    Phase 1.0

--- main.py
+++ main.py
@@ -6,23 +6,18 @@ def webhook(request):
     response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Internal Error']}}]})
     
     #get request payload
-    json_parse = request.get_json() #jadi dictionary
-    print(type(json_parse)) 
-    print(type(request))
-    print(json_parse.get('originalDetectIntentRequest', {}).get('source') is 'telegram')
-    print(json_parse.get('originalDetectIntentRequest', {}).get('source') == 'telegram')
+    json_parse = request.get_json() 
     
     #dialogflow and telegram validation
     if ('responseId' in json_parse) and (json_parse.get('originalDetectIntentRequest', {}).get('source') == 'telegram') :
-        '''
+        
         try:
-            url = ""
-            response = req.post(url,request)
+            url = "https://us-central1-newagent-mnsmjh.cloudfunctions.net/ChatGateway"
+            response = req.post(url,json_parse)
         except:
             print("Error Something Happen....")
-            response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Internal Error']}}]})
-        '''
-
+            response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Request Error']}}]})
+        
         response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Sukses']}}]})
     else:
         response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Illegal Action']}}]})
