073bb25e413ab3d0a6b92b8f6f181645f78830ee
unknown <hafidhfikri.rasyid@gmail.com>
2020-05-02 20:40:48 +0700

    Phase 1.0

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ def webhook(request):
     json_parse = request.get_json() 
     
     #dialogflow and telegram validation
-    if ('responseId' in json_parse) and (json_parse.get('originalDetectIntentRequest', {}).get('source') == 'telegram') :
-        
+    if ('responseId' in json_parse) and (json_parse.get('originalDetectIntentRequest', {}).get('source') == 'telegram') :    
         try:
             url = "https://us-central1-newagent-mnsmjh.cloudfunctions.net/ChatGateway"
             response = req.post(url,json_parse)
         except:
             print("Error Something Happen....")
             response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Request Error']}}]})
-        
-        response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Sukses']}}]})
     else:
         response = json.dumps({'fulfillmentMessages':[{'text':{'text':['Illegal Action']}}]})
     
